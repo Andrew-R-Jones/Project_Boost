96cd159cb6fe2443346314153a776ac9b2cccb18
Andrew Jones <andrew_52490@yahoo.com>
2019-01-18 10:40:46 -0700

    Code Refactored. Added to thrust and rotation for more natural consistent movement.

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -5,49 +5,59 @@ using UnityEngine;
 
 public class Rocket : MonoBehaviour {
 
-    Rigidbody rigidbody;
+    // SerializeField, editable in the inspector in unity, not other scripts
+    [SerializeField] float rcsThrust = 100f; // rcs, reaction control system
+    [SerializeField] float mainThrust = 100f;
+
+
+    Rigidbody rigidBody;
     AudioSource audio;
 
 
 
     // Use this for initialization
     void Start () {
-        rigidbody = GetComponent<Rigidbody>();
+        rigidBody = GetComponent<Rigidbody>();
         audio = GetComponent<AudioSource>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        ProcessInput();
+        Thrust();
+        Rotate();
 	}
 
-    private void ProcessInput()
+    private void Thrust()
     {
         if (Input.GetKey(KeyCode.Space)) // can thrust while rotating
         {
-            rigidbody.AddRelativeForce(Vector3.up);
+
+            rigidBody.AddRelativeForce(Vector3.up * mainThrust);
             if (!audio.isPlaying)
-            audio.Play();
+                audio.Play();
         }
         else
         {
             audio.Stop();
         }
+    }
 
+    private void Rotate()
+    {
+        rigidBody.freezeRotation = true; // take manual control of rotation
+
+        float rotationThisFrame = rcsThrust * Time.deltaTime;
 
         if (Input.GetKey(KeyCode.A))
         {
             print("Rotating Left");
-            transform.Rotate(Vector3.forward);
-
+            transform.Rotate(Vector3.forward * rotationThisFrame);
         }
         else if (Input.GetKey(KeyCode.D))
         {
-            print("Rotating Right");
-            transform.Rotate(-Vector3.forward);
-           
+            print("Rotating right");
+            transform.Rotate(-Vector3.forward * rotationThisFrame);
         }
-
-
+        rigidBody.freezeRotation = false;
     }
 }
